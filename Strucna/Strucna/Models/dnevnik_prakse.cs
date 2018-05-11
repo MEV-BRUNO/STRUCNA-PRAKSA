c748f8b34d31e49054fda81ad45dd7c65384d790
YDundie <mustacmarko@yahoo.com>
2018-05-11 08:51:50 +0200

    Initial commit

---
+++ Strucna/Strucna/Models/dnevnik_prakse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Strucna.Models
+{
+    public class Dnevnik_prakse
+    {
+        public int id_praksa { get; set; }
+        public int id_student { get; set; }
+        public int red_broj { get; set; }
+        public DateTime datum { get; set; }
+        public string naslov { get; set; }
+        public string opis { get; set; }
+    }
+}
---
+++ Strucna/Strucna/Models/dokumenti.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Strucna.Models
+{
+
+    public class Dokumenti
+    {
+        public int id_dokument { get; set; }
+        public string naziv { get; set; }
+        public string put { get; set; }
+
+    }
+}
