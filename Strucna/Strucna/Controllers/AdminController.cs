1f285f5907b5e01b5a9ce4e90b53c3b721f41a1d
YDundie <mustacmarko@yahoo.com>
2018-05-13 17:55:05 +0200

    Promjena index stranice i općenito poboljšavanje solutiona

---
+++ Strucna/Strucna/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Strucna.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult index_admin()
+        {
+            return View();
+        }
+    }
+}
--- Strucna/Strucna/Models/mentor.cs
+++ Strucna/Strucna/Models/mentor.cs
@@ -5,10 +5,13 @@ using System.Web;
 
 namespace Strucna.Models
 {
-    public class Mentor
-    {
+
+    public class Mentor { 
+
         public int id_mentor { get; set; }
+
         public string ime_prezime { get; set; }
+
         public string email { get; set; }
         public string lozinka { get; set; }
         public int id_studij { get; set; }
