4d65b8720ee355fe05bf0bb2d83d4d328511cf43
Splantic <38259041+Splantic@users.noreply.github.com>
2018-05-14 01:12:41 +0200

    Dodan Login controller
    Dodan login controller

---
+++ Strucna/Strucna/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Strucna.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
