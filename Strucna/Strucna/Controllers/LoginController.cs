f9d1916ddad68e17ec7dfb1c1731b9d7ec7d3f96
YDundie <mustacmarko@yahoo.com>
2018-05-15 00:34:41 +0200

    Povezivanje s bazom i funcionalnost registracije mentora

--- Strucna/Strucna/Controllers/LoginController.cs
+++ Strucna/Strucna/Controllers/LoginController.cs
@@ -1,14 +1,16 @@
-﻿using System;
+﻿using Strucna.Baza_povezivanje;
+using Strucna.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
 namespace Strucna.Controllers
 {
     public class LoginController : Controller
     {
-        // GET: Login
+        private strucnapraksa baza = new strucnapraksa();
+
         public ActionResult lozinka()
         {
            
@@ -16,21 +18,57 @@ namespace Strucna.Controllers
             return View();
         }
 
-        public ActionResult reg()
-        {
-            
 
-            return View();
+        [HttpGet]
+        public ActionResult reg_mentor()
+        {
+            Mentor m = new Mentor();
+
+            return View(m);
         }
 
+        public ActionResult reg_mentor( Mentor m)
+        {
+            if (ModelState.IsValid)
+            {
 
+                baza.Mentori.Add(m);
+                baza.SaveChanges();
+                return RedirectToAction("login");
+
+            }
+
+            return View(m);
+        }
+
+        [HttpGet]
+        public ActionResult reg_student()
+        {
+            Student s = new Student();
+
+            return View(s);
+        }
+
+        [HttpGet]
         public ActionResult login()
         {
-            
+            Mentor men = new Mentor();
 
-            return View();
+            return View(men);
         }
 
+        [HttpPost]
 
+        public ActionResult login(Mentor m)
+        {
+            foreach (Mentor men in baza.Mentori)
+            {
+                if (men.lozinka == m.lozinka && men.email == m.email)
+                {
+                    return RedirectToAction("AdminIndex");
+                }
+            }
+            return RedirectToAction("Prijava");
+        }
     }
 }
