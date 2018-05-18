e4208d38b4cbef757df051b74a2bf62dfff4b4d8
PetarZidar <petar.zidar@studen.mev.hr>
2018-05-18 18:16:21 +0200

    Html u Razor i update modela

---
+++ Strucna/Strucna/Baza_povezivanje/strucnapraksa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Web.Mvc;
+using Strucna.Models;
+
+namespace Strucna.Baza_povezivanje
+{
+    public class strucnapraksa : DbContext
+    {
+ 
+            public DbSet<Mentor> Mentori { get; set; }
+    }
+    }
--- Strucna/Strucna/Models/mentor.cs
+++ Strucna/Strucna/Models/mentor.cs
@@ -1,19 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace Strucna.Models
 {
+    [Table("mentor")]
+    public class Mentor {
 
-    public class Mentor { 
-
+        [Required]
+        [Key]
         public int id_mentor { get; set; }
 
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} je obavezan podatak")]
+        [Display(Name = "Ime i Prezime")]
         public string ime_prezime { get; set; }
 
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} je obavezan podatak")]
+        [Display(Name = "Email adresa")]
         public string email { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} je obavezan podatak")]
+        [Display(Name = "Lozinka")]
         public string lozinka { get; set; }
+      
+        [Display(Name = "ID studij")]
         public int id_studij { get; set; }
 
     }
