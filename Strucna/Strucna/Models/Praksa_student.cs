82a25b3d8582668f4e92a72f0f68ea607ba96418
YDundie <mustacmarko@yahoo.com>
2018-05-26 11:00:09 +0200

    Updatanje sa projektom

--- Strucna/Strucna/Models/Praksa_student.cs
+++ Strucna/Strucna/Models/Praksa_student.cs
@@ -1,18 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace Strucna.Models
 {
+    [Table("praksa_student")]
     public class Praksa_student
     {
+        [Required]
+        [Key]
         public int id_praksa { get; set; }
         public int id_student { get; set; }
         public int id_poduzece { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} je obavezan podatak")]
+        [Display(Name = "Od")]
         public DateTime datum_od { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} je obavezan podatak")]
+        [Display(Name = "Do")]
         public DateTime datum_do { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} je obavezan podatak")]
+        [Display(Name = "Odobreno")]
         public int odobreno { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} je obavezan podatak")]
+        [Display(Name = "Ocjena")]
         public int ocjena { get; set; }
     }
 }
--- Strucna/Strucna/Models/ocjena.cs
+++ Strucna/Strucna/Models/ocjena.cs
@@ -1,23 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace Strucna.Models
+
 {
+    [Table("ocjena")]
     public class Ocjena
     {
+        [Required]
+        [Key]
         public int id_praksa { get; set; }
         public int id_student { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} je obavezan podatak")]
+        [Display(Name = "Datum")]
         public DateTime datum { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} je obavezan podatak")]
+        [Display(Name = "Predati izvješće")]
         public int izvjesce_predano { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} je obavezan podatak")]
+        [Display(Name = "Dokumentirano izvješće")]
         public string izvjesce_dokument { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} je obavezan podatak")]
+        [Display(Name = "Predati potvrdu")]
         public int potvrda_predano { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} je obavezan podatak")]
+        [Display(Name = "Dokumentirana potvrda")]
         public string potvrda_dokument { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} je obavezan podatak")]
+        [Display(Name = "Predati anketu")]
         public int anketa_predano { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} je obavezan podatak")]
+        [Display(Name = "Dokumentirana Anketu")]
         public string anketa_dokument { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} je obavezan podatak")]
+        [Display(Name = "Predani zahtjev")]
         public int zahtjev_predano { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} je obavezan podatak")]
+        [Display(Name = "Dokumentirani zahtjev")]
         public string zahtjev_dokument { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} je obavezan podatak")]
+        [Display(Name = "Ocjena")]
         public string ocjena { get; set; }
     }
 }
